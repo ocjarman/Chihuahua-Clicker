cb60522e6cb2c4294afa2fa44f4207bbae0e84a0
Olivia Jarman <ocjarman@gmail.com>
2022-09-02 20:54:58 -0400

    making so no zoom on phone

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
 /* eslint-disable quotes */
 // when chihuahua img clicked, add one to num chihuahuas
-let chihuahuaCount = 0;
-document.getElementById("attackOverlay").style.display = "none";
-
-const divToBeClicked = document.getElementById("chihuahuaContainer");
-let numberToAdd = document.querySelector("h2");
-
-function moreChihuahuas() {
-  chihuahuaCount += 1;
-  numberToAdd.innerHTML = `Number of Karats: ${chihuahuaCount}`;
-  console.log(chihuahuaCount);
-  // if (document.getElementById("picToChange").src === chihuahuaPics[9]) {
-  //   chihuahuaCount = 0;
-  //   console.log("youve been attacked");
-  // }
-}
-// when div is clicked, run more chihuahuas
-divToBeClicked.addEventListener("click", moreChihuahuas);
-
-setInterval(moreChihuahuas, 500);
-
-const wayMoreButton = document.getElementById("wayMore");
-
-wayMoreButton.addEventListener("click", wayMoreChihuahuas);
-
-function wayMoreChihuahuas() {
-  chihuahuaCount += 100;
-  numberToAdd.innerHTML = `Number of Karats: ${chihuahuaCount}`;
-  console.log(chihuahuaCount);
-  changePic();
-}
-
-///////////////////***********dont alter above code************//////////////
-// console.log("randomChiPicture-->", randomChiChiPicture.innerHTML);
-
-// make a helper function to change the chihuahua pic
-
-let chihuahuaPics = [
+const chihuahuaPics = [
   "./imagesOfChihuahuas/chi1.jpg",
   "./imagesOfChihuahuas/chi2.jpg",
   "./imagesOfChihuahuas/chi3.jpeg",
@@ -64,30 +28,44 @@ let chihuahuaPics = [
   "./imagesOfChihuahuas/chi24.jpeg",
 ];
 
+let chihuahuaCount = 0;
+document.getElementById("attackOverlay").style.display = "none";
+
+const divToBeClicked = document.getElementById("chihuahuaContainer");
+const numberToAdd = document.querySelector("h2");
+
+function moreChihuahuas() {
+  chihuahuaCount += 1;
+  numberToAdd.innerHTML = `Number of Karats: ${chihuahuaCount}`;
+}
+// when div is clicked, run more chihuahuas
+divToBeClicked.addEventListener("click", moreChihuahuas);
+
+// more karats will populate every 500 ms by 1
+setInterval(moreChihuahuas, 500);
+
 function changePic() {
   const randomChi = Math.floor(Math.random() * chihuahuaPics.length);
   document.getElementById("picToChange").src = chihuahuaPics[randomChi];
   if (randomChi === 9) {
     chihuahuaCount = 0;
     document.getElementById("attackOverlay").style.display = "block";
-    console.log("youve been attacked");
   } else {
     document.getElementById("attackOverlay").style.display = "none";
   }
 }
 
+// when clicked, change pic
 divToBeClicked.addEventListener("click", changePic);
 
-// add later:
-// buy a lot of chihuahuas at once button += 5
+// if you want way more chihuahuas
+const wayMoreButton = document.getElementById("wayMore");
 
-// $(".hole").click(function () {
-//   //if the class is currently mole, we want to add one to score and remove the mole
-//   if ($(this).hasClass("hole mole")) {
-//     score += 1;
-//     $(this).removeClass("mole");
-//   }
-//   $("#score").html(score);
-// });
+// this fxn gives a lot more chihuahuas & changes the pic when clicked
+function wayMoreChihuahuas() {
+  chihuahuaCount += 100;
+  numberToAdd.innerHTML = `Number of Karats: ${chihuahuaCount}`;
+  changePic();
+}
 
-function newBackgroundColor() {}
+wayMoreButton.addEventListener("click", wayMoreChihuahuas);
